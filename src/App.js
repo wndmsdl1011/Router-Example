bfc98718c9820d857403907d8b959adc79aefcce
wndmsdl1011 <jueun7547@naver.com>
2024-07-15 01:51:38 +0900

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
+import { useState } from 'react';
 import './App.css';
 import Homepage from './page/Homepage';
 import Aboutpage from './page/Aboutpage';
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
+import LoginPage from './page/LoginPage';
+import UserPage from './page/UserPage';
 
 function App() {
+  const [authenticate,setAuthenticate]=useState(true)
+  const PrivateRoute = () => {
+    return authenticate == true?<UserPage/>:<Navigate to="/login"/>
+  }
+
   return (
     <div>
       <Routes>
@@ -14,6 +21,8 @@ function App() {
         <Route path='/about' element={<Aboutpage/>}/>
         <Route path='/products' element={<ProductPage/>} />
         <Route path='/products/:id' element={<ProductDetailPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/user' element={<PrivateRoute/>} />
       </Routes>      
     </div>
   );
--- src/page/ProductPage.js
+++ src/page/ProductPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useSearchParams } from 'react-router-dom'
 const ProductPage = () => {
+  let [query,setQuery] = useSearchParams()
+  console.log("dddd",query.get("q"))
   return (
     <div>
       <h1>Show All Products</h1>
